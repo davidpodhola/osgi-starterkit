16e891d8e61c4806d45ece5e3094b04be70941d9
davidpodhola <david@podhola.net>
2018-06-19 10:55:29 +0200

    integration tests started

---
+++ integration_tests/test/common.js
@@ -0,0 +1,11 @@
+var chakram = require('chakram');
+var chai = require('chai');  
+var assert = chai.assert;    // Using Assert style
+var expect = chai.expect;    // Using Expect style
+
+module.exports = {
+    nothing: function() {
+    },
+
+    API_ROOT = "http://localhost:8010/"
+}
---
+++ integration_tests/test/test_example.js
@@ -0,0 +1,22 @@
+var chakram = require('chakram');
+var common = require('./common');
+
+var chai = require('chai');  
+var assert = chai.assert;    // Using Assert style
+var expect = chai.expect;    // Using Expect style
+
+var API_ROOT = common.API_ROOT;
+
+describe("Example", function() {
+    it("should return status", function () {
+        chakram.get(API_ROOT + "/example/status" ).
+        done( function(response) {
+            var body = response.body;
+            expect( body ).to.contain( "Blah Blah Blah" );
+            expect( body ).to.contain( "This is active Java code with server time" );
+        })    
+    });
+    it("should just work", function () {
+        common.nothing();
+    });
+});
